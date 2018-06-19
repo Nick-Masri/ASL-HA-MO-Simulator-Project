7bf5d6f3e3ec7fdc329ff3aa717795c8e4104a29
pense2 <jackschilling@live.com>
2018-06-19 15:24:09 -0700

    calc d_time

--- helpers.py
+++ helpers.py
@@ -13,3 +13,8 @@ def CreateDict(list):
     for x in list:
         dict[x[0]] = Asset(x[0], x[1])
     return dict
+
+def calc_d_time(origin, destination, o_time):
+    travel_time = graph[o][d]
+    d_time = travel_time + o_time
+    return d_time
