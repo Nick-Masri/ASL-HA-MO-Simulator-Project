5ffb9d78474e039cd722d873babc0da61e04039f
Nicholas Masri <nicholasmasri@gmail.com>
2018-06-19 10:30:32 -0700

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,99 @@
+class Employee:
+    def __init__(self, id, current_position, status='IDLE', origin=-1, destination=-1, o_time=-1, d_time=-1):
+        self.id = id
+        self.status = status
+        self.origin = origin
+        self.destination = destination
+        self.o_time = o_time
+        self.d_time = d_time
+        self.current_position = current_position
+    
+    # Get Methods
+    def get_id(self):
+        return self.id
+    def get_status(self):
+        return self.status
+    def get_origin(self):
+        return self.origin
+    def get_destination(self):
+        return self.destination
+    def get_o_time(self):
+        return self.o_time
+    def get_d_time(self):
+        return self.d_time
+    def get_current_position(self):
+        return self.current_position
+    
+    # Mutator Methods
+    def change_status(self, s):
+        self.status = s
+    def change_origin(self, o):
+        self.origin = o
+    def change_destination(self, d):
+        self.destination = d
+    def change_o_time(self, ot):
+        self.o_time = ot
+    def change_d_time(self, dt):
+        self.d_time = dt
+    def change_current_position(self, cp):
+        self.current_position = cp
+
+
+class Car:
+    def __init__(self, id, current_position, status='IDLE', origin=-1, destination=-1, o_time=-1, d_time=-1):
+        self.id = id
+        self.status = status
+        self.origin = origin
+        self.destination = destination
+        self.o_time = o_time
+        self.d_time = d_time
+        self.current_position = current_position
+        
+    # Get Methods
+    def get_id(self):
+        return self.id
+    def get_status(self):
+        return self.status
+    def get_origin(self):
+        return self.origin
+    def get_destination(self):
+        return self.destination
+    def get_o_time(self):
+        return self.o_time
+    def get_d_time(self):
+        return self.d_time
+    def get_current_position(self):
+        return self.current_position
+
+    # Mutator Methods
+    def change_status(self, s):
+        self.status = s
+    def change_origin(self, o):
+        self.origin = o
+    def change_destination(self, d):
+        self.destination = d
+    def change_o_time(self, ot):
+        self.o_time = ot
+    def change_d_time(self, dt):
+        self.d_time = dt
+    def change_current_position(self, cp):
+        self.current_position = cp
+
+class Station:
+    def __init__(self, id, waiting_customers, available_cars):
+        self.id = id
+        self.waiting_customers = waiting_customers
+        self. available_cars = available_cars
+    # Get Methods
+    def get_id(self):
+        return self.id
+    def get_waiting_customers(self):
+        return self.waiting_customers
+    def get_available_cars(self):
+        return self.available_cars
+    
+    # Mutator Methods
+    def change_waiting_customers(self, wc):
+        self.waiting_customers = wc
+    def change_available_cars(self, ac):
+        self.available_cars = ac
---
+++ main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+from functions import *
+
+# Initializing Time
+time = (00,00,00) # Time Starts at 0
+
+# Initializing Setup Variables (Random rn)
+employee_list = [[1, 2], [2, 5], [3, 7], [4, 10]]
+car_list = [[1, 1], [2, 2], [3, 2], [4, 4], [5, 4], [6, 6], [7, 9], [8,10]]
+map = [[0,5,8,11,13,13,12,11,8,5], [5,0,5,8,11,13,13,12,11,8], [8,5,0,5,8,11,13,13,12,11], [11,8,5,0,5,8,11,13,13,12], [12,11,8,5,0,5,8,11,13,13],
+       [13,12,11,8,5,0,5,8,11,13], [13,13,12,11,8,5,0,5,8,11], [11,13,13,12,11,8,5,0,5,8], [8,11,13,13,12,11,8,5,0,5], [5,8,11,13,13,12,11,8,5,0]]
+station_list = []
+employee_dict = {}
+
+for i in range(1,11):
+    station_list.append((i))
+# Calling the Functions
+
+for x in employee_list:
+    employee_dict["e_{0}".format(x[0])] = Employee(x[0], x[1])
+
+print employee_dict
+
+e_3 = Employee(employee_list[3][0], employee_list[3][1])
+
+'''
+for time[0] < 24:
+    for car in
+
+
+
+    time[0] += 01
+    
+    '''
+'''from functions.py import *
+
+for current_time in :
+For loop through the time of day (for current_time in day_time:)
+For loop through the cars
+Check if destination_time == current_time
+Change status of employee from rebalancing to idle
+Check if any of the employees are idle:
+'''
