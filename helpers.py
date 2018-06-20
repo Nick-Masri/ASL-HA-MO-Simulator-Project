7ff9d736ac2b0127e9f84304e57d21f49c5c972e
Matt Cooley <mwcooley99@gmail.com>
2018-06-20 15:22:26 -0700

    Added in loop one in the update loop

--- helpers.py
+++ helpers.py
@@ -1,12 +1,24 @@
 from classes import *
 from globals import *
 
-def checkEveryMinute(station_dict, employee_requests, customer_requests):
+def checkEveryMinute(station_dict, employee_requests, customer_requests, current_time):
 
     for station in station_dict:
+        curr_station = station_dict[station]
         # Loop 1
-        for person in station.get_en_route_list():
-            pass
+        for person in curr_station.get_en_route_list():
+            curr_car_list = station.get_car_list()
+            if person.get_desintation_time() == current_time:
+                curr_vehicle_id = person.get_verichle_id()
+                if curr_vehicle_id != None:
+                    curr_car_list.append(person.get_verichle_id())
+                if isinstance(Person(), Person):  # Is it a customer?
+                    # get rid of person object
+                    pass
+                else:  # If employee then reset that employee and add to the employee queue
+                    person.reset()
+                    employee_list.append(person)
+
 
         # Loop 2
         for employee_request in station.get_employee_list():
@@ -30,4 +42,4 @@ def create_dict(_list):
     return _dict
 
 def calc_d_time(origin, destination, o_time):
-    return GRAPH_var[origin][destination] + o_time
+    return GRAPH_VAR[origin][destination] + o_time
