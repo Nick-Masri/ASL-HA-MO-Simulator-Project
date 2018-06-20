963b8ebd61512b3b73727f029bb08ed702955f69
masrin123@hotmail.com <masrin123@hotmail.com>
2018-06-20 14:27:31 -0700

    added in useful comments:

--- helpers.py
+++ helpers.py
@@ -4,17 +4,25 @@ from globals import *
 def checkEveryMinute(station_dict, employee_requests, customer_requests):
 
     for station in station_dict:
+        # Go through employee_requests to only have employees at this station
+        # Go through customer_requests to only have employees at this station
+
+        curr_station = station_dict[station]
+
+        employee_list = curr_station.get_employee_list()
+        customer_list = curr_station.get_customer_list()
+
         # Loop 1
-        for person in station.get_en_route_list():
+        for person in curr_station.get_en_route_list():
             pass
 
         # Loop 2
         for employee_request in employee_requests:
-            station.assign_employee(station.get_employee_list(employee_requests.index(employee_request)), employee_request)
+            station.assign_employee(employee_list[employee_requests.index(employee_request)], employee_request)
 
         # Loop 3
         for customer_request in customer_requests:
-            station.append_customer_list(customer_request)
+            customer_list.append(customer_request)
 
         # Loop 4
         for request in (station.get_request_list()):
