4fb4afadc020a5dc5437162d994551d87b0698e4
pense2 <jackschilling@live.com>
2018-06-20 13:39:49 -0700

    redo

--- classes.py
+++ classes.py
@@ -1,61 +1,106 @@
-class Asset:
-    def __init__(self, asset_id,  current_position, status='IDLE', origin=None, destination=None, o_time=None, d_time=None):
-        self.asset_id = asset_id # Employee ID number
-        self.status = status # 0 = IDLE, 1 = en route, 2 = rebalancing
-        self.origin = origin # IDLE = None
-        self.destination = destination # IDLE = None
-        self.o_time = o_time # IDLE = None
-        self.d_time = d_time # IDLE = None
-        self.current_position = current_position # if IDLE = current station number
-    
+import globals
+
+class Person:
+    def __init__(self, origin, destination, origin_time, destination_time, current_position, vehicle_id = None):
+        self.origin = origin
+        self.destination = destination
+        self.origin_time = origin_time
+        self.destination_time = origin_time + globals.GRAPH_VAR[origin][destination]
+        self.current_position = current_position
+        self.vehicle_id = vehicle_id
+ 
     # Get Methods
-    def get_id(self):
-        return self.asset_id
-    def get_status(self):
-        return self.status
     def get_origin(self):
         return self.origin
     def get_destination(self):
         return self.destination
-    def get_o_time(self):
-        return self.o_time
-    def get_d_time(self):
-        return self.d_time
+    def get_origin_time(self):
+        return self.origin_time
+    def get_destination_time(self):
+        return self.destination_time
     def get_current_position(self):
         return self.current_position
-    
+    def get_vehicle_id(self):
+        return self.vehicle_id
+
     # Mutator Methods
-    def change_status(self, s):
-        self.status = s
-    def change_origin(self, o):
+    def change_origin(self,o):
         self.origin = o
     def change_destination(self, d):
         self.destination = d
-    def change_o_time(self, ot):
-        self.o_time = ot
-    def change_d_time(self, dt):
-        self.d_time = dt
+    def change_origin_time(self, ot):
+        self.origin_time = ot
+    def change_destination_time(self, dt):
+        self.destination_time = dt
     def change_current_position(self, cp):
         self.current_position = cp
-
-
+    def change_vehicle_id(self, v):
+        self.vehicle_id = v
+        
+class Employee(Person):
+    def __init__(self, origin, destination, origin_time, destination_time, current_position, vehicle_id = None, emp_id):
+        Person.__init__(self, origin, destination, origin_time, destination_time, current_position, vehicle_id = None)
+        self.emp_id = emp_id
+    
+    # Get Methods
+    def get_origin(self):
+        return self.origin
+    def get_destination(self):
+        return origin_time + globals.GRAPH_VAR[origin][destination]
+    def get_origin_time(self):
+        return self.origin_time
+    def get_destination_time(self):
+        return self.destination_time
+    def get_current_position(self):
+        return self.current_position
+    def get_vehicle_id(self):
+        return self.vehicle_id
+    def get_emp_id(self):
+        return self.emp_id
+    
+    # Mutator Methods
+    def change_origin(self,o):
+        self.origin = o
+    def change_destination(self, d):
+        self.destination = d
+    def change_origin_time(self, ot):
+        self.origin_time = ot
+    def change_destination_time(self, dt):
+        self.destination_time = dt
+    def change_current_position(self, cp):
+        self.current_position = cp
+    def change_vehicle_id(self, v):
+        self.vehicle_id = v
+    def change_emp_id(self, e):
+        self.emp_id = e
+        
 class Station:
-    def __init__(self, station_id, available_cars=0, waiting_customers=0):
+    def __init__(self, station_id, cars, list_new_employees, list_waiting_customers, list_en_route):
         self.station_id = station_id
-        self.available_cars = available_cars
-        self.waiting_customers = waiting_customers
+        self.cars = cars
+        self.list_new_employees = list_new_employees
+        self.list_waiting_customers = list_waiting_customers
+        self.list_en_route = list_en_route
 
     # Get Methods
     def get_id(self):
         return self.station_id
-    def get_waiting_customers(self):
-        return self.waiting_customers
-    def get_available_cars(self):
-        return self.available_cars
+    def get_cars(self):
+        return self.cars
+    def get_list_new_employees(self):
+        return self.list_new_employees
+    def get_list_waiting_customers(self):
+        return self.list_waiting_customers
+    def get_list_en_route(self):
+        return self.list_en_route
     
     # Mutator Methods
-    def change_waiting_customers(self, wc):
-        self.waiting_customers = wc
-    def change_available_cars(self, ac):
-        self.available_cars = ac
+    def change_cars(self, c):
+        self.cars = c
+    def change_list_new_employees(self, l):
+        self.list_new_employees = l
+    def change_list_waiting_customers(self, w):
+        self.list_waiting_customers = w
+    def change_list_en_route(self, er):
+        self.list_en_route = er
 
