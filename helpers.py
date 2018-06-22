dbf0fdc98396c81acfd5ab6f1d66b2917f50bd70
masrin123@hotmail.com <masrin123@hotmail.com>
2018-06-22 11:09:47 -0700

    Added functions to main loop in helpers

--- helpers.py
+++ helpers.py
@@ -1,6 +1,66 @@
 from classes import *
 from globals import *
 
+def arrivals(arrival_list, time, cars, employees):
+    for person in arrival_list:
+        if person.get_destination_time() == time:
+
+            arrival_list.remove(person)
+            current_vehicle_id = person.get_vehicle_id()
+
+            if current_vehicle_id is not None:
+                cars.append(current_vehicle_id)
+
+            if isinstance(person, Employee):
+                person.reset()
+                employees.append(person)
+            else:
+                del person
+
+def assign_drivers(requests, time, cars, employee, station):
+    for driver_request in requests:
+        if driver_request[2] == time:
+            try:
+                current_car = cars.pop(0)
+
+                current_employee = employee.pop(0)
+                current_employee.update_status(driver_request, current_car)
+                station[driver_request[1]].append_en_route_list(current_employee)
+            except IndexError:
+                # Save the Employee instructions
+                print('Not enough cars for the employees')
+                break
+        else:
+            break
+
+def assign_pedestrians(requests, time, employee, station):
+    for pedestrian_request in requests:
+        if pedestrian_request[2] == time:
+            current_employee = employee.pop(0)
+            current_employee.update_status(pedestrian_request)
+            station[pedestrian_request[1]].append_en_route_list(current_employee)
+        else:
+            break
+
+def update_customer_list(requests, time, list):
+    for customer_request in requests:
+        if customer_request[2] == time:
+            customer = Person(customer_request[0], customer_request[1], customer_request[0])
+            list.append(customer)
+        else:
+                break
+
+def assign_customers(list, cars, station_dictionary):
+    for customer in list:
+        try:
+            current_car = cars.pop(0)
+
+            current_customer = list.pop(0)
+            current_customer.update_status(customer, current_car)
+            station_dictionary[customer[1]].get_en_route_list().append(current_customer)
+        except IndexError:
+            print('Not enough cars for the customers')
+            break
 
 def check_every_minute(station_dict, driver_requests, pedestrian_requests, customer_requests, current_time):
 
@@ -11,64 +71,19 @@ def check_every_minute(station_dict, driver_requests, pedestrian_requests, custo
         current_station = station_dict[station]
         current_car_list = current_station.get_car_list()
         employee_list = current_station.get_employee_list()
-        customer_list = current_station.get_waiting_customers()
-        en_route_list = current_station.get_en_route_list()
+        customer_list = current_station.get_waiting_customers(True)
+        en_route_list = current_station.get_en_route_list(True)
 
-        # Looping through arrivals
-        for person in en_route_list:
-            if person.get_destination_time() == current_time:
+        # Loop Arrivals
+        arrivals(en_route_list, current_time, current_car_list, employee_list)
 
-                en_route_list.remove(person)
-                current_vehicle_id = person.get_vehicle_id()
+        # Assign Employees
+        assign_drivers(driver_requests, current_time, current_car_list, employee_list, current_station)
+        assign_pedestrians(pedestrian_requests, current_time, employee_list, current_station)
 
-                if current_vehicle_id is not None:
-                    current_car_list.append(current_vehicle_id)
-
-                if isinstance(person, Employee):
-                    person.reset()
-                    employee_list.append(person)
-                else:
-                    del person
-
-        # Looping through driver requests and assigning them
-        for driver_request in driver_requests:
-            if driver_request[2] == current_time:
-                try:
-                    current_car = current_car_list.pop(0)
-
-                    current_employee = employee_list.pop(0)
-                    current_employee.update_status(driver_request, current_car)
-                    station_dict[driver_request[1]].append_en_route_list(current_employee)
-                except IndexError:
-                    # Save the Employee instructions
-                    print('Not enough cars for the employees')
-                    break
-            else:
-                break
-
-        # Looping through pedestrian requests and assigning them
-        for pedestrian_request in pedestrian_requests:
-            current_employee = employee_list.pop(0)
-            current_employee.update_status(pedestrian_request)
-            station_dict[pedestrian_request[1]].append_en_route_list(current_employee)
-
-        # Appending customer requests
-        for customer_request in customer_requests:
-            customer_list.append(customer_request)
-
-        # Sending out customers
-        for customer in customer_list:
-            try:
-                current_car = current_car_list.pop(0)
-
-                current_customer = customer_list.pop(0)
-                current_customer.update_status(customer, current_car)
-                station_dict[customer[1]].get_en_route_list().append(current_customer)
-            except IndexError:
-                    print('Not enough cars for the customers')
-                    break
-
-
+        # Update Customer list and Assign Them
+        update_customer_list(customer_requests, current_time, customer_list)
+        assign_customers(customer_list, current_car_list, station_dict)
 
 def instructions_every_five_minutes():
     pass
