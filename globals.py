794a92baa94f974d2455f97eb2b00b178db2df32
pense2 <jackschilling@live.com>
2018-06-22 11:39:48 -0700

    car_list and station_list fix

--- globals.py
+++ globals.py
@@ -12,20 +12,28 @@ GRAPH_VAR = [[0,5,8,11,13,13,12,11,8,5],  # needs to be 50x50 eventually
 
 # Initializing Setup Variables (Random rn)
 # [Employee ID #, Station ID #]
-EMPLOYEE_LIST = [[1, 2],
-                 [2, 5],
-                 [3, 7],
-                 [4, 10]]
+EMPLOYEE_LIST = [[1, 5],
+                 [2, 12],
+                 [3, 36],
+                 [4, 47]]
 
-# [Car ID #, Station ID #]
-CAR_LIST = [[1, 1],
-            [2, 2],
-            [3, 2],
-            [4, 4],
-            [5, 4],
-            [6, 6],
-            [7, 9],
-            [8,10]]
+# [Car ID #, Station ID #], contains cars with unique IDs 
+# (250 total, 0-249) assigned 5 to each station (50 total, 0-49)
+CAR_LIST = []
+temp_limit = 5
+station_temp_id = 0
+car_temp_id = 0
+while car_temp_id <= temp_limit:
+    CAR_LIST.append([car_temp_id, station_temp_id])
+    car_temp_id = car_temp_id + 1
+    if car_temp_id == 250:
+        break
+    if car_temp_id % 5 == 0:
+        station_temp_id = station_temp_id + 1
+        temp_limit = temp_limit + 5
+    
+#print(CAR_LIST)
+#print(len(CAR_LIST))
 
 
 # Form of [origin][destination] = # of people requesting that route
@@ -33,4 +41,5 @@ PERSON_LIST = [[0,1,0],
                [2,0,3],
                [0,1,0]]
 
-STATION_LIST = [num for num in range(11)]
+#STATION_LIST contains 51 stations, starting at 0, going to 50
+STATION_LIST = [num for num in range(51)]
