0f13f9bea8cca107e3fec1a505689dbea2612eb4
masrin123@hotmail.com <masrin123@hotmail.com>
2018-06-30 20:25:04 -0700

    tried to fix errors

--- helpers.py
+++ helpers.py
@@ -21,30 +21,32 @@ def arrivals(arrival_list, time, cars, employees):
 
 def assign_drivers(requests, time, cars, employee, station):
     for driver_request in requests:
-        try:
-            current_car = cars.pop(0)
-
-            current_employee = employee.pop(0)
-            current_employee.update_status(driver_request, current_car)
-            station[driver_request[1]].append_en_route_list(current_employee)
-        except IndexError:
-            # Save the Employee instructions
-            print('Not enough cars for the employees')
-            break
+        # try:
+        #     current_car = cars.pop(0)
+        #
+        #     current_employee = employee.pop(0)
+        #     current_employee.update_status(driver_request, current_car)
+        #     station[driver_request[1]].append_en_route_list(current_employee)
+        # except IndexError:
+        #     # Save the Employee instructions
+        #     print('Not enough cars for the employees')
+        #     break
+        break
 
 
 def assign_pedestrians(requests, time, employee, station):
     for pedestrian_request in requests:
-        current_employee = employee.pop(0)
-        current_employee.update_status(pedestrian_request)
-        station[pedestrian_request[1]].append_en_route_list(current_employee)
+    #     current_employee = employee.pop(0)
+    #     current_employee.update_status(pedestrian_request)
+    #     station[pedestrian_request[1]].append_en_route_list(current_employee)
+        break
 
 
-def update_customer_list(requests, time, list):
+def update_customer_list(requests, time, cust_list):
     for customer_request in requests:
         # if customer_request[2] == time:  <-- I don't think we need this anymore.
         customer = Person(customer_request[0], customer_request[1], time)
-        list.append(customer)
+        cust_list.append(customer)
 
 
 def assign_customers(customer_list, cars, station_dictionary):
@@ -56,7 +58,7 @@ def assign_customers(customer_list, cars, station_dictionary):
                 current_customer.update_status(customer, current_car)
                 station_dictionary[customer.get_destination()].get_en_route_list().append(current_customer)
             except IndexError:
-                print('No car current_customer: {}'.format(customer.get_origin()))
+                print('No car for customer {}'.format(customer.get_origin()))
                 print(cars)
                 print(customer_list)
                 break
@@ -83,7 +85,7 @@ def update(station_dict, driver_requests, pedestrian_requests, customer_requests
 
         overload = 50 - (len(current_station.get_car_list()) + len(current_station.get_en_route_list()))
 
-        if overload <= 0:
+        if overload < 0:
             errors.append("Station {0}  will have {1} more cars than it can allow".format(current_station, -overload))
 
         # Assign Employees
