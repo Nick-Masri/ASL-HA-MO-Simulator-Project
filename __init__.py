b5daa05cd7d57c17ade799c2177110bbd98f0efc
masrin123@hotmail.com <masrin123@hotmail.com>
2018-06-30 20:00:05 -0700

    played around with print message

--- __init__.py
+++ __init__.py
@@ -18,22 +18,16 @@ for station in STATION_MAPPING_INT:
 
 state = []
 # Full loop of 24 hours of checks
+
 for time in range(len(CUST_REQUESTS)):
     print("Time: {}".format(time))
+
     driver_requests = format_instructions(time, load_instructions('driver'))
     pedestrian_requests = format_instructions(time, load_instructions('pedestrian'))
     customer_requests = CUST_REQUESTS[time]
-    temp = update(station_dict, driver_requests, pedestrian_requests, customer_requests, time)
-    temp_station = {}
-    stations = temp[0]  # The station dict
-    for station in stations:
-        temp_station[station] = {'Cars': stations[station].get_car_list(),
-                                 'Waiting Customers': len(stations[station].get_employee_list()),
-                                 # There's clearly an issue here. The enroute list is always length: 58!!
-                                 'en_route_list': len(stations[station].get_en_route_list()),
-                                 'self.request_list': len(stations[station].get_request_list())}
-    print(temp_station)
-    break
+    
+    for station in station_dict:
+        print('There are {0} cars at station: {1}'.format(len(station_dict[station].get_car_list()), station))
 
 
     
