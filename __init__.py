db0b33ceaafdd8bcc3c183594b312cc0530880f5
pense2 <jackschilling@live.com>
2018-07-11 12:59:45 -0700

    state fixes

--- __init__.py
+++ __init__.py
@@ -17,10 +17,13 @@ for station in STATION_MAPPING_INT:
             emp_list.append(emps[0])
     station_dict[station] = Station(station, car_list, emp_list)
 
-for time in range(len(CUST_REQUESTS)):
+for time in range(len(CUST_REQUEST)):
     print("Time: {}".format(time))
     output.append("\nTime: {}".format(time))
     output.append('------------------------------------------------------')
+    
+    iVehicles = []
+    iDrivers = []
 
     driver_requests = format_instructions(time, load_instructions('driver'))
     pedestrian_requests = format_instructions(time, load_instructions('pedestrian'))
@@ -33,16 +36,20 @@ for time in range(len(CUST_REQUESTS)):
         output.append('\t\tNumber of Idle Vehicles: {}'.format(len(station_dict[station].get_car_list())))
         output.append('\t\tAvailable Parking: {}'.format(50 - len(station_dict[station].get_car_list())))
         output.append('\t\tNumber of People En_Route: {}'.format(len(station_dict[station].get_en_route_list())))
-
+        iVehicles.append(len(station_dict[station].get_car_list()))
+        iDrivers.append(len(station_dict[station].get_employee_list()))
 
     # station = 5
     # output.append('\tStation: {}'.format(station))
     # output.append('\t\tNumber of Idle Vehicles: {}'.format(len(station_dict[station].get_car_list())))
     # output.append('\t\tAvailable Parking: {}'.format(50 - len(station_dict[station].get_car_list())))
     # output.append('\t\tNumber of People En_Route: {}'.format(len(station_dict[station].get_en_route_list())))
-
+    
+    STATE = {'idleVehicles': iVehicles, 'idleDrivers': iDrivers, 'privateVehicles': 0}
+    print(STATE)
+    
     output.append('Errors: {}'.format(errors))
-
+    
 
 output_file = open('output.txt', 'w')
 
@@ -57,3 +64,5 @@ for x in CUST_REQUESTS:
     request_file.write('{}\n'.format(x))
 
 request_file.close()
+
+
