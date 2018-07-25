266c14b9afbdbafbdde6b6b534894182b9a209d6
masrin123@hotmail.com <masrin123@hotmail.com>
2018-07-25 11:25:15 -0700

    fixed update.py

--- main.py
+++ main.py
@@ -1,197 +1,54 @@
 #!/usr/bin/python
-
-import argparse
-import update
-
-def __main__():
-     # Ask for user input
-    contrller_type = argparse
-
-     update()
+import simulator.run
 
 
+def main():
+    print("What controller do you want to run?")
+    controller_type = input("N/Naive or S/Smart:\n")
+    controller_types = ("N", "Naive", "S", "Smart")
 
+    if controller_type in controller_types:
+        simulator.run(controller_type)
+    else:
+        print("Not a Valid Input")
+        main()
 
-
-
-
+if __name__ == '__main__':
+    main()
 
 
 
 ######################################
 # Main Loop ~ NM
 ######################################
+#
+#
+# raw_requests = np.load('./data/10_days/hamo10days.npy')
+# cust_requests = format_instructions(raw_requests)
+# driver_requests = [[] for i in range(len(station_dict))]
+# pedestrian_requests = [[] for i in range(len(station_dict))]
+#
 
-
-raw_requests = np.load('./data/10_days/hamo10days.npy')
-cust_requests = format_instructions(raw_requests)
-driver_requests = [[] for i in range(len(station_dict))]
-pedestrian_requests = [[] for i in range(len(station_dict))]
-
-for time in range(0, len(cust_requests)):
-    print("Time: {}".format(time))
-    text_file_output.append("\nTime: {}".format(time))
-    text_file_output.append('------------------------------------------------------')
-
-    idle_vehicles = []
-    idle_drivers = []
-
-    vehicle_arrivals = np.zeros(shape=(len(station_dict), 12))
-    driver_arrivals = np.zeros(shape=(len(station_dict), 12))
-
-    customer_requests = cust_requests[time]
-
-    errors = main.Update(station_dict, customer_requests, time, driver_requests, pedestrian_requests)
-    # for station in station_dict:
-    #     print('******************')
-    #     print(station_dict[station].car_list)
-    #     print('*******************')
-    # Logging current state
-    for station in sorted(station_dict):
-
-        ######################################
-        # Writing to text_file_output Files ~ NM
-        ######################################
-
-        text_file_output.append('\tStation: {}'.format(station))
-        text_file_output.append('\t\tNumber of Idle Vehicles: {}'.format(len(station_dict[station].car_list)))
-        text_file_output.append('\t\tAvailable Parking: {}'.format(50 - len(station_dict[station].car_list)))
-        text_file_output.append('\t\tNum People En_Route: {}'.format(len(station_dict[station].get_en_route_list())))
-
-        ############################################
-        # Setting Up Idle Vehicles and Drivers ~ JS
-        ############################################
-
-        idle_vehicles.append(len(station_dict[station].car_list))
-        idle_drivers.append(len(station_dict[station].employee_list))
-
-        ########################################
-        # Updating Vehicle/Driver Arrivals ~ NM
-        ########################################
-
-        for person in station_dict[station].get_en_route_list(True):
-            for i in range(time, time + 12):
-                # if person.vehicle_id is not None:
-                #     if person.destination_time == i:
-                #         if isinstance(person, Employee):
-                #             driver_arrivals[station][i - time] += 1
-                #
-                #         vehicle_arrivals[station][i - time] += 1
-                #         break
-                if person.destination_time == i:
-                    if isinstance(person, Employee):
-                        driver_arrivals[station][i - time] += 1
-                    if person.vehicle_id is not None:
-                        vehicle_arrivals[station][i - time] += 1
-                else:
-                    break
-
-        ########################################
-        # Fraction of Time for at Capacity or Empty ~ JS
-        ########################################
-
-        num_parked_cars = len(station_dict[station].car_list)
-        num_park_spots = 50 - len(station_dict[station].car_list)
-
-        if num_parked_cars == 0:
-            total_time_empty[station] += 1
-
-        if num_parked_cars == num_park_spots:
-            total_time_full[station] += 1
-
-    ######################################
-    # Creating Forecast Dictionary ~ NM/MC
-    ######################################
-    if controller_type == 'smart':
-        Forecast = {
-            # 'demand' : demand_forecast_parser(time), # ~ MC
-            'demand': demand_forecast_parser_alt(time),
-            'vehicleArrivals': vehicle_arrivals,  # ~ NM
-            'driverArrivals': driver_arrivals,  # ~ NM
-        }
-
-        # print("FORECAST")
-        # for k, v in Forecast.items():
-        #     print(k, v.shape)
-
-        ######################################
-        # Creating State Dictionary ~ JS
-        ######################################
-
-        State = {
-            'idleVehicles': np.array(idle_vehicles),
-            'idleDrivers': np.array(idle_drivers),
-            'privateVehicles': np.zeros((58, 1))
-        }
-
-        # Fake data RoadNetwork
-        # RoadNetwork = np.load("./roadNetwork.npy").item()
-
-        # create controller if it doesn't already exist
-        try:
-            controller
-        except:
-            controller = MoDController(RoadNetwork)
-
-        # Other Fake State data for testing.
-        # Parameters = np.load("./parameters.npy").item()
-        # State = np.load("./state.npy").item()
-        # Forecast = np.load("./forecast.npy").item()
-        # Flags = np.load("./flags.npy").item()
-
-        [tasks, controller_output] = controller.computerebalancing(Parameters, State, Forecast, Flags)
-        # for task in tasks:
-        #     print(task)
-        #
-        # for c_output in controller_output:
-        #     print(c_output)
-
-    elif controller_type == 'naive':
-
-        if morningStart <= time and time <= morningEnd:
-            morning_rebalancing(station_dict)
-            morningStart += 24
-            morningEnd += 24
-        elif eveningStart <= time and time <= eveningEnd:
-            evening_rebalancing(station_dict)
-            eveningStart += 24
-            eveningEnd += 24
-
-    print('\n\n*****************************\n\n')
-
-    text_file_output.append('Errors: {}'.format(errors))
-
-    pedestrian_requests = tasks['driverRebalancingQueue']
-    # for request in pedestrian_requests:
-    #     print(request)
-    vehicle_requests = tasks['vehicleRebalancingQueue']
-    print(pedestrian_requests)
-    print(vehicle_requests)
-
-    text_file_output.append('Errors: {}'.format(errors))
-
-    # driver_requests = format_instructions(text_file_output_requests)
-    # customer_requests = format_instructions(text_file_output_requests)
-
-######################################
-# Tracking Errors / Summing Errors ~ JS
-######################################
-
-sum_station_no_park_errors = np.sum(no_park_errors, axis=0)  # no parking errors per station total
-sum_station_no_car_cust_errors = np.sum(no_car_cust_errors, axis=0)  # no car available for customers errors per station
-sum_station_no_car_emp_errors = np.sum(no_car_emp_errors, axis=0)  # no car available for employees errors per station
-
-sum_time_no_park_errors = np.sum(no_park_errors, axis=1)  # no parking errors per time total
-sum_time_no_car_cust_errors = np.sum(no_car_cust_errors, axis=1)  # no car available for customers errors per time total
-sum_time_no_car_emp_errors = np.sum(no_car_emp_errors, axis=1)  # no car available for employees errors per time total
-
-######################################
-# Writing to text_file_output File ~ NM
-######################################
-
-text_file_output_file = open('text_file_output.txt', 'w')
-
-for item in text_file_output:
-    text_file_output_file.write("%s\n" % item)
-
-text_file_output_file.close()
+#
+# ######################################
+# # Tracking Errors / Summing Errors ~ JS
+# ######################################
+#
+# sum_station_no_park_errors = np.sum(no_park_errors, axis=0)  # no parking errors per station total
+# sum_station_no_car_cust_errors = np.sum(no_car_cust_errors, axis=0)  # no car available for customers errors per station
+# sum_station_no_car_emp_errors = np.sum(no_car_emp_errors, axis=0)  # no car available for employees errors per station
+#
+# sum_time_no_park_errors = np.sum(no_park_errors, axis=1)  # no parking errors per time total
+# sum_time_no_car_cust_errors = np.sum(no_car_cust_errors, axis=1)  # no car available for customers errors per time total
+# sum_time_no_car_emp_errors = np.sum(no_car_emp_errors, axis=1)  # no car available for employees errors per time total
+#
+# ######################################
+# # Writing to text_file_output File ~ NM
+# ######################################
+#
+# text_file_output_file = open('text_file_output.txt', 'w')
+#
+# for item in text_file_output:
+#     text_file_output_file.write("%s\n" % item)
+#
+# text_file_output_file.close()
--- simulator/update.py
+++ simulator/update.py
@@ -1,11 +1,12 @@
-import state_tracker
-import parameters
-import
+import simulator.state_tracker
+import simulator.parameters
+import numpy as np
+
 
 class Update:
 
-    def __init__(self, current_time, station_dict, customer_requests, driver_requests, pedestrian_requests):
-        self.current_time = current_time
+    def __init__(self, time, station_dict,customer_requests, driver_requests, pedestrian_requests):
+        self.time = time
         self.station_dict = station_dict
         self.customer_requests = customer_requests
         self.driver_requests = driver_requests
@@ -15,6 +16,7 @@ class Update:
         self.no_car_cust_errors = np.zeros(shape=(2880, 58))
         self.no_park_errors = np.zeros(shape=(2880, 58))
         self.no_car_emp_errors = np.zeros(shape=(2880, 58))
+        self.state_tracker = simulator.state_tracker
 
     def loop(self):
         for station in sorted(self.station_dict):
@@ -25,39 +27,39 @@ class Update:
             self.en_route_list = station.get_en_route_list(True)
 
             # Loop Arrivals
-            self.arrivals(self.en_route_list, self.current_time, self.car_list, self.employee_list, self.station)
+            self.arrivals(self.en_route_list, self.time, self.car_list, self.employee_list, self.station)
 
             # Check for Errors ******** This is assuming the capacity is 50 for each station ***********
-            overload = station.available_parking - (len(current_station.car_list) + len(current_station.get_en_route_list()))
+            overload = station.available_parking - (len(self.car_list) + len(self.en_route_list))
 
             if overload < 0:
                 self.errors.append(
-                    "Station {0} will have {1} more cars than it can allow".format(current_station, -overload))
-                self.no_park_errors[current_time][current_station] += 1
+                    "Station {0} will have {1} more cars than it can allow".format(self.station, -overload))
+                self.no_park_errors[self.time][self.station] += 1
 
             # Put customers into cars
-            if len(customer_requests) > 0:
+            if len(self.customer_requests) > 0:
                 # Update Customer list and Assign Them
-                for customer_request in customer_requests:
+                for customer_request in self.customer_requests:
                     if customer_request[0] == station:
                         # add to station cust waiting list
-                        self.update_customer_list(customer_request, current_time, customer_list)
+                        self.update_customer_list(customer_request, self.time, self.customer_list)
 
                         print("CUSTOMER REQUEST: {}".format(customer_request))
 
                 # assigns customers to cars if available
-                self.assign_customers(customer_list, current_car_list, station_dict, errors, current_time)
+                self.assign_customers(self.customer_list, self.current_car_list, self.station_dict, self.errors, self.time)
 
-            if len(driver_requests[station]) > 0 or len(pedestrian_requests[station]) > 0:
+            if len(self.driver_requests[station]) > 0 or len(self.pedestrian_requests[station]) > 0:
 
 
                 # Assign drivers
-                self.assign_drivers(current_station, driver_requests[station], station_dict, errors, current_time)
+                self.assign_drivers(self.station, self.driver_requests[station], self.station_dict, self.errors, self.time)
 
                 # Assign Pedestrians
-                self.assign_pedestrians(current_station, pedestrian_requests[station], station_dict, current_time)
+                self.assign_pedestrians(self.station, self.pedestrian_requests[station], self.station_dict, self.time)
 
-        return errors
+        return self.errors
 
     def arrivals(self):
         while len(self.en_route_list) > 0:
@@ -68,7 +70,7 @@ class Update:
                 current_vehicle_id = person.vehicle_id
                 if current_vehicle_id is not None:
                     self.car_list.append(current_vehicle_id)
-                if isinstance(person, classes.Employee):
+                if isinstance(person, self.state_tracker.Employee):
                     person.reset()
                     self.employee_list.append(person)
                 else:
@@ -83,27 +85,27 @@ class Update:
             try:
                 current_car = self.station.car_list.pop(0)
                 driver = self.station.employee_list.pop(0)
-                driver.update_status(self.station.station_id, destination, self.current_time, current_car)
+                driver.update_status(self.station.station_id, destination, self.time, current_car)
                 self.station_dict[driver.destination].append_en_route_list(driver)
             except IndexError:
                 self.errors.append('No car for employee at Station Number {}'.format(driver.origin))
-                self.no_car_emp_errors[self.current_time, driver.origin] += 1
+                self.no_car_emp_errors[self.time, driver.origin] += 1
                 break
 
-
     def assign_pedestrians(self):
-        for destination in pedestrian_tasks:
-            ped = station.employee_list.pop(0)
-            ped.update_status(station.station_id, destination, current_time)
-            station_dictionary[ped.destination].append_en_route_list(ped)
+        station = self.station
+        station_dict = self.station_dict
 
+        for destination in self.pedestrian_requests:
+            ped = station.employee_list.pop(0)
+            ped.update_status(station.station_id, destination, self.time)
+            station_dict[ped.destination].append_en_route_list(ped)
 
     def update_customer_list(self, requests, time, cust_list):
-        customer = classes.Person(requests[0], requests[1], time)
+        customer = self.state_tracker.Person(requests[0], requests[1], time)
         cust_list.append(customer)
 
-
-    def assign_customers(self, customer_list, cars, station_dictionary, errors, current_time):
+    def assign_customers(self, customer_list, cars, station_dictionary, errors, time):
         while len(customer_list) > 0:
             customer = customer_list[0]
             try:
@@ -113,5 +115,5 @@ class Update:
                 station_dictionary[customer.destination].append_en_route_list(customer)
             except IndexError:
                 errors.append('No car for customer at Station Number {}'.format(customer.origin))
-                self.no_car_cust_errors[current_time, customer.origin] += 1
+                self.no_car_cust_errors[time, customer.origin] += 1
                 break
