b0579c4e60fc7a0b9eb2016b57fcca51197b697d
masrin123@hotmail.com <masrin123@hotmail.com>
2018-07-25 16:14:26 -0700

    added update status

--- simulator/people.py
+++ simulator/people.py
@@ -16,6 +16,13 @@ class Person:
         self.current_position = [origin, destination]
         self.vehicle_id = vehicle_id
 
+    def update_status(self, request, new_car=None):
+        self.origin = request.origin
+        self.destination = request.destination
+        self.origin_time = request.origin_time
+        self.vehicle_id = new_car
+
+
 class Employee(Person):
     def __init__(self, origin, destination, origin_time, vehicle_id=None):
         Person.__init__(self, origin, destination, origin_time, vehicle_id=None)
@@ -28,15 +35,15 @@ class Employee(Person):
         self.destination_time = None
         self.vehicle_id = None
 
-    def update_status(self, origin, destination, origin_time, new_car=None):
-        self.origin = origin
-        self.destination = destination
-        self.origin_time = origin_time
-        self.vehicle_id = new_car
-        if new_car is not None:
-            self.destination_time = origin_time + get_travel_time(hamo_travel_times, origin, destination)
-        else:
-            self.destination_time = origin_time + get_travel_time(walking_travel_times, origin, destination)
+    # def update_status(self, origin, destination, origin_time, new_car=None):
+    #     self.origin = origin
+    #     self.destination = destination
+    #     self.origin_time = origin_time
+    #     self.vehicle_id = new_car
+    #     if new_car is not None:
+    #         self.destination_time = origin_time + get_travel_time(hamo_travel_times, origin, destination)
+    #     else:
+    #         self.destination_time = origin_time + get_travel_time(walking_travel_times, origin, destination)
 
 def get_travel_time(time_graph, origin, destination):
     if origin == destination:
